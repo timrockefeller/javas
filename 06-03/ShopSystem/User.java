2362000b66cf17e180428a3c629a0c199140229b
Tim Rockefeller <tim720719@163.com>
2018-06-08 09:41:39 +0000

    up

--- 06-03/ShopSystem/User.java
+++ 06-03/ShopSystem/User.java
@@ -1,3 +1,8 @@
 public class User {
     
+    public 
+    
+    public String userName;
+    public int userId;
+    public String userPassWord;a
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Java Projects
+
+in the oracle class
+for damn basic
